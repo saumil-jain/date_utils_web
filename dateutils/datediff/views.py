f53cb6b94c592d4da3f1d1c187794c28b26dfc3d
Saumil Jain <saumil-jain@users.noreply.github.com>
2018-03-27 22:21:18 +0530

    Extract and print input date in the difference_from_today view method
    This input date is extracted from the request.POST object
    which has the form data.
    For now, in case of POST request, the page is rendered again.

--- dateutils/datediff/views.py
+++ dateutils/datediff/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def difference_from_today(request):
     if request.method == "POST":
-        pass
+        from_date = request.POST.get("from_date")
+        if from_date:
+            print("From date : {}".format(from_date))
+        else:
+            print("No date entered")
+        return render(request, "datediff/date_diff.html")
     else:
         return render(request, "datediff/date_diff.html")
