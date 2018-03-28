8c05b69d3f151bcacb1f369dc4e6e0848cbc9291
Saumil Jain <saumil-jain@users.noreply.github.com>
2018-03-28 12:33:43 +0530

    In datediff models.py, add methods to parse input date and calculate difference from today's date
    The input date should be in the format yyyy-dd-mm, as returned
    by the date HTML field.
    The date difference method returns the absolute difference between
    the input date and today's date.

--- dateutils/datediff/models.py
+++ dateutils/datediff/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+import datetime
+
+
+def process_input_date(input_date):
+    """Parse the input date string and return a date object
+
+    The object returned is of the type datetime.date.
+    :param input_date: The date in the ISO format yyyy-mm-dd
+    :return: A datetime.date object representing the input date
+    :raise IndexError and ValueError
+    """
+    try:
+        input_date_data = input_date.split("-")
+        year = int(input_date_data[0])
+        month = int(input_date_data[1])
+        day = int(input_date_data[2])
+        return datetime.date(year=year, month=month, day=day)
+    except (IndexError, ValueError) as e:
+        print("Invalid date format")
+        raise
+
+
+def calculate_date_diff_from_today(date_object):
+    """Calculates the absolute difference between input date and today's date.
+
+    :param date_object: The datetime.date object representing the input date
+    :return: the absolute difference of the two dates as a datetime.timedelta object.
+    """
+    today = datetime.date.today()
+    difference = abs(today - date_object)
+    return date_object, today, difference
