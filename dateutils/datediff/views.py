010ed5a0cd74738a913afcc4ae0aafdc92b152a6
Saumil Jain <saumil-jain@users.noreply.github.com>
2018-03-28 13:47:58 +0530

    In datediff view method, find difference between input date and today's date, set output in context
    Call the methods from the datediff model to parse the input date and
    find the difference between input date and today's date.
    Set the output message string in the template context.

--- dateutils/datediff/views.py
+++ dateutils/datediff/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .models import process_input_date, calculate_date_diff_from_today
+
 
 # Create your views here.
 def difference_from_today(request):
@@ -7,8 +9,14 @@ def difference_from_today(request):
         from_date = request.POST.get("from_date")
         if from_date:
             print("From date : {}".format(from_date))
+            from_date, today, difference = calculate_date_diff_from_today(process_input_date(from_date))
+            output_text = "The difference between the date {} and today's date {} is {} days.".format(from_date,
+                                                                                                      today,
+                                                                                                      difference.days)
+            context = {"output": output_text}
+            print(output_text)
         else:
             print("No date entered")
-        return render(request, "datediff/date_diff.html")
+        return render(request, "datediff/date_diff.html", context=context)
     else:
         return render(request, "datediff/date_diff.html")
