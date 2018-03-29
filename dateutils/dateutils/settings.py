550c9b03b8afb2cc98456573f5542c49a3507a1a
Saumil Jain <saumil-jain@users.noreply.github.com>
2018-03-29 18:10:36 +0530

    Add templates dir in setting.py for project level templates
    The project level templates, such as the site home page, will
    reside in dateutils/templates.

--- dateutils/dateutils/settings.py
+++ dateutils/dateutils/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'dateutils.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "dateutils", "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
