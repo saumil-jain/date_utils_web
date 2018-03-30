80a00e8aef4e0a5d9e4d68fde0b98811301121a9
Saumil Jain <saumil-jain@users.noreply.github.com>
2018-03-30 19:05:25 +0530

    Refactor difference_from_today view name
    Refactor in the url config and templates

--- dateutils/datediff/urls.py
+++ dateutils/datediff/urls.py
@@ -4,5 +4,5 @@ from .views import difference_from_today
 
 
 urlpatterns = [
-    re_path(r"^days_till_today$", difference_from_today, name="datediff_days_till_today")
+    re_path(r"^difference_from_today$", difference_from_today, name="datediff_difference_from_today")
 ]
