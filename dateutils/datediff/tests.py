afb84bf38de5426aa8c7bc7dd97d96f3726890af
Saumil Jain <saumil-jain@users.noreply.github.com>
2018-04-11 00:22:50 +0530

    Add test method for add_days_to_date model method
    The test method is added to tests.py and tests the addition of
    days to a date.

--- dateutils/datediff/tests.py
+++ dateutils/datediff/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import add_days_to_date
+
+import datetime
+
 
 # Create your tests here.
+class DateDiffTests(TestCase):
+
+    def test_add_days_to_date(self):
+        date = datetime.date(year=2018, month=2, day=10)
+        days = 5
+        expected_date = datetime.date(year=2018, month=2, day=15)
+        computed_date = add_days_to_date(date_object=date, days=days)
+        self.assertEqual(computed_date, expected_date)
