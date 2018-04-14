94066fa7019937966958e8ea3b4353aecd810a84
Saumil Jain <saumil-jain@users.noreply.github.com>
2018-04-14 18:53:17 +0530

    Use operation form value to add or subtract days from date
    Based on the operation (add or subtract) the days' sign is changed.

--- dateutils/datediff/views.py
+++ dateutils/datediff/views.py
@@ -32,11 +32,15 @@ def add_days_to_date(request):
     if request.method == "POST":
         date = request.POST.get("from_date")
         days = int(request.POST.get("days"))
+        operation = request.POST.get("operation")
         context = None
         if date and days:
             try:
-                new_date = models_add_days_to_date(process_input_date(date), days)
-                output_text = "New date is {}".format(new_date)
+                if operation == "-":
+                    days = -days
+                input_date = process_input_date(date)
+                new_date = models_add_days_to_date(input_date, days)
+                output_text = "{} {} {} days is {}".format(input_date, operation, abs(days), new_date)
             except ValueError as e:
                 output_text = "Invalid date format {}. {}".format(date,
                                                                      "Date must be between 0001-01-01 and 9999-12-31")
