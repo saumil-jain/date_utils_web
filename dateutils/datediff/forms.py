25e173f0f629cb95772a341fdf787963a8565a43
Saumil Jain <saumil-jain@users.noreply.github.com>
2018-04-14 23:09:41 +0530

    Add form class AddDaysToDateForm
    This form class defines three form fields:
    A date field, a choice field for operation, an integer field for days.

--- dateutils/datediff/forms.py
+++ dateutils/datediff/forms.py
@@ -3,3 +3,11 @@ from django import forms
 
 class DateDiffFromTodayForm(forms.Form):
     from_date = forms.DateField(label="Enter date")
+
+
+class AddDaysToDateForm(forms.Form):
+    input_date = forms.DateField(label="Enter date")
+    operation = forms.ChoiceField(choices=[("+", "Add (+)"), ("-", "Subtract (-)")])
+    days = forms.IntegerField(min_value=1, max_value=3652058)
+    # The max value is difference in days between
+    # datetime.date.max and datetime.date.min
