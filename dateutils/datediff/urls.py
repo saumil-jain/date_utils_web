d330848a61e9a3c2e5fbe39aba562bd9cbdc3d04
Saumil Jain <saumil-jain@users.noreply.github.com>
2018-04-15 15:51:37 +0530

    Add url entry and home page link for difference between two dates feature

--- dateutils/datediff/urls.py
+++ dateutils/datediff/urls.py
@@ -1,9 +1,10 @@
 from django.urls import re_path
 
-from .views import difference_from_today, add_days_to_date
+from .views import difference_from_today, add_days_to_date, diff_dates
 
 
 urlpatterns = [
     re_path(r"^difference_from_today$", difference_from_today, name="datediff_difference_from_today"),
-    re_path(r"^add_days$", add_days_to_date, name="datediff_add_days")
+    re_path(r"^add_days$", add_days_to_date, name="datediff_add_days"),
+    re_path(r"^diff_dates$", diff_dates, name="datediff_diff_dates")
 ]
